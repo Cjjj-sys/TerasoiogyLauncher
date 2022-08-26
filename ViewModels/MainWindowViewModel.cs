1573484bfd7cc4b53063df805644545295f30c02
kangkang <2646381627@qq.com>
2022-08-26 22:54:03 +0800

    init viewmodels

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Windows;
+
+namespace TerasoiogyLauncher.ViewModels
+{
+    [ObservableObject]
+    public partial class MainWindowViewModel
+    {
+        [RelayCommand]
+        public void Close()
+        {
+            Environment.Exit(0);
+        }
+        [RelayCommand]
+        public void Minimum()
+        {
+            
+        }
+    }
+}
