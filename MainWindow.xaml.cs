54d8c5773149a09cc5c26301b37a9e94aae6def0
kangkang <2646381627@qq.com>
2022-08-26 22:51:23 +0800

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace TerasoiogyLauncher
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleBorder_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
