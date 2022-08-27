8d38e94b7e8a06c56a119b46d0c89149d47602e3
kangkang <2646381627@qq.com>
2022-08-27 14:58:22 +0800

    😊实现了导航页面的功能 🎉窗口基本布局完成

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,10 +10,29 @@ namespace TerasoiogyLauncher.ViewModels
     [ObservableObject]
     public partial class MainWindowViewModel
     {
+        public MainWindowViewModel()
+        {
+            _currentViewModel = _homeViewModel;
+        }
+        private HomeViewModel _homeViewModel = new();
+        private DownloadGameViewModel _downloadGameViewModel = new();
+
+        [ObservableProperty]
+        private INotifyPropertyChanged _currentViewModel;
+
         [RelayCommand]
         public void Close() => Task.Run(() => Environment.Exit(0));
         [RelayCommand]
 #pragma warning disable CS8602 // 解引用可能出现空引用。
         public void Minimum(object window) => (window as MainWindow).WindowState = WindowState.Minimized;
+        [RelayCommand]
+        public void NavigateTo(string page)
+        {
+            CurrentViewModel = page switch
+            {
+                "Home" => _homeViewModel,
+                "DownloadGame" => _downloadGameViewModel
+            };
+        }
     }
 }
