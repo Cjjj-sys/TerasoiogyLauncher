ee399f1683854032497825bf316e499dca55759e
kangkang <2646381627@qq.com>
2022-08-29 16:25:26 +0800

    🎉基本完成NimLaucnher命令封装

--- Services/INimLauncherCliService.cs
+++ Services/INimLauncherCliService.cs
@@ -6,7 +6,8 @@ namespace TerasoiogyLauncher.Services
     public interface INimLauncherCliService
     {
         Task DownloadGameAsync(string version, Action<string, double?, string?> progressBarAction);
-
+        Task DownloadJREAsync(string version, Action<string, double?, string?> progressBarAction);
         Task<bool> InitAsync();
+        Task InstallGameAsync(string version, Action<string> action);
     }
 }
--- Services/NimLauncherCliService.cs
+++ Services/NimLauncherCliService.cs
@@ -5,13 +5,25 @@ using System.Threading.Tasks;
 
 namespace TerasoiogyLauncher.Services;
 
+/// <summary>
+/// 对Nim启动器各种命令的封装服务
+/// </summary>
 public class NimLauncherCliService : INimLauncherCliService
 {
+    /// <summary>
+    /// Nim启动器的文件名
+    /// </summary>
+    private const string NIMLAUNCHER_FILENAME = "Main.exe";
+
+    /// <summary>
+    /// 初始化命令的封装
+    /// </summary>
+    /// <returns></returns>
     public async Task<bool> InitAsync()
     {
         try
         {
-            await Cli.Wrap("Main.exe").WithArguments("init").ExecuteAsync();
+            await Cli.Wrap(NIMLAUNCHER_FILENAME).WithArguments("init").ExecuteAsync();
             return true;
         }
         catch (Exception)
@@ -20,9 +32,52 @@ public class NimLauncherCliService : INimLauncherCliService
         }
     }
 
+    /// <summary>
+    /// 下载游戏命令的封装
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="progressBarAction"></param>
+    /// <returns></returns>
     public async Task DownloadGameAsync(string version, Action<string, double?, string?> progressBarAction)
     {
-        var cli = Cli.Wrap("Main.exe").WithArguments(new string[] { "download_game", version }) | PipeTarget.ToDelegate(output =>
+        await DownloadAsync(version, progressBarAction);
+    }
+
+    /// <summary>
+    /// 安装游戏命令的封装
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="action"></param>
+    /// <returns></returns>
+    public async Task InstallGameAsync(string version, Action<string> action)
+    {
+        var cli = Cli.Wrap(NIMLAUNCHER_FILENAME).WithArguments("install_game") | PipeTarget.ToDelegate(output =>
+        {
+            action.Invoke(output);
+        });
+        await cli.ExecuteBufferedAsync();
+    }
+
+    /// <summary>
+    /// 下载JRE命令的封装
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="progressBarAction"></param>
+    /// <returns></returns>
+    public async Task DownloadJREAsync(string version, Action<string, double?, string?> progressBarAction)
+    {
+        await DownloadAsync(version, progressBarAction);
+    }
+
+    /// <summary>
+    /// 提取可复用的下载方法
+    /// </summary>
+    /// <param name="version"></param>
+    /// <param name="progressBarAction"></param>
+    /// <returns></returns>
+    private static async Task DownloadAsync(string version, Action<string, double?, string?> progressBarAction)
+    {
+        var cli = Cli.Wrap(NIMLAUNCHER_FILENAME).WithArguments(new string[] { "download_jre", version }) | PipeTarget.ToDelegate(output =>
         {
             double? progress = null;
             string? speed = null;
